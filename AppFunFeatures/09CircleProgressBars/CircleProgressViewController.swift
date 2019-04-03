c3be56992f9073133bf176e921bc3e216f213e27
XinZouS <sanking.saphira@foxmail.com>
2019-04-03 16:26:31 -0400

    set txt layer

--- AppFunFeatures/09CircleProgressBars/CircleProgressViewController.swift
+++ AppFunFeatures/09CircleProgressBars/CircleProgressViewController.swift
@@ -13,10 +13,10 @@ class CircleProgressViewController: UIViewController {
     
     let slider = UISlider(frame: .zero)
     
-    let layerOne = CALayer()
-    let layerTwo = CALayer()
-    let layerThree = CALayer()
-    let layerFour = CALayer()
+    let layerOne = ProgressLayer()
+    let layerTwo = ProgressLayer()
+    let layerThree = ProgressLayer()
+    let layerFour = ProgressLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +46,56 @@ class CircleProgressViewController: UIViewController {
     }
     
     @objc private func sliderValueChanged() {
-        
+        layerOne.number = Double(slider.value)
+        layerTwo.number = Double(slider.value)
+        layerThree.number = Double(slider.value)
+        layerFour.number = Double(slider.value)
     }
     
+}
+
+// 圆形进度条的父类，用于显示百分比文本
+class ProgressLayer: CALayer {
+    
+    var number: Double = 0.0 {
+        didSet {
+            txtLayer.string = String(format: "%.01f%%", number * 100) // show 66.6%
+            txtLayer.setNeedsDisplay()
+        }
+    }
+    
+    let txtLayer: CATextLayer = {
+        let l = CATextLayer()
+        let font = UIFont.systemFont(ofSize: 12)
+        l.font = font.fontName as CFTypeRef
+        l.fontSize = font.pointSize
+        l.foregroundColor = UIColor.blue.cgColor
+        l.alignmentMode = CATextLayerAlignmentMode.center
+        l.contentsScale = UIScreen.main.scale
+        l.isWrapped = true
+        l.string = ""
+        return l
+    }()
+    
+    override init() {
+        super.init()
+        addSublayer(txtLayer)
+    }
+    
+    override init(layer: Any) {
+        super.init(layer: layer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        
+        let atts = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
+        let tH = NSString(string: "100%").boundingRect(with: CGSize(width: CGFloat.infinity, height: CGFloat.infinity), options: .usesLineFragmentOrigin, attributes: atts, context: nil).height
+        
+        txtLayer.frame = CGRect(x: 0, y: frame.height * 0.5 - tH * 0.5, width: frame.width, height: tH)
+    }
 }
